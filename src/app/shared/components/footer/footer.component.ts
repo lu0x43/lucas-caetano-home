82647005ae76dfe6025f80acadd0464eccd12307
LucasCaetano <lucas_caetano123@hotmail.com>
2023-06-18 23:03:50 -0300

    adjust footer email and instagram

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   info = {
-    email: 'lucascaetano@contato.com',
+    email: 'contato@devlucascaetano.com',
     phone: '+55(44)99708-7793',
     whatsapp: '5544997087793',
     linkedin: 'https://www.linkedin.com/in/lucas-caetano-/',
     github: 'https://github.com/devLucasCaetano',
-    instagram: '',
+    instagram: 'https://www.instagram.com/dev.lucascaetano/',
   };
 
   constructor() {}
