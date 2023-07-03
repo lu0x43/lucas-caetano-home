31b6b6f53709883266846a351270c6074457dd74
LucasCaetano <lucas_caetano123@hotmail.com>
2023-07-03 00:03:07 -0300

    feat: implements type Writer in roles header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -12,11 +12,17 @@ export class HeaderComponent implements OnInit {
   nameHeader = 'Lucas Caetano';
 
   rolesHeader: string[] = ['ROLES.WEB_DEV', 'ROLES.MOBILE_DEV'];
+  currentRoleIndex: number = 0;
+  currentRole: string = '';
 
   constructor(
     public translateService: TranslateService,
     private authenticationService: AuthService
   ) {}
+
+  ngOnInit(): void {
+    this.typeWriter();
+  }
 
   changeLang(): void {
     if (this.translateService.currentLang === 'en') {
@@ -28,5 +34,45 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {}
+  typeWriter(): void {
+    const roleKey = this.rolesHeader[this.currentRoleIndex];
+    this.translateService.get(roleKey).subscribe((translatedRole: string) => {
+      const roleLength = translatedRole.length;
+      let i = 0;
+      const typeInterval = setInterval(() => {
+        if (i < roleLength) {
+          this.currentRole = translatedRole.slice(0, i + 1);
+          i++;
+        } else {
+          clearInterval(typeInterval);
+          setTimeout(() => {
+            this.deleteWriter();
+          }, 1000);
+        }
+      }, 100);
+    });
+  }
+
+  deleteWriter(): void {
+    const roleKey = this.rolesHeader[this.currentRoleIndex];
+    this.translateService.get(roleKey).subscribe((translatedRole: string) => {
+      const roleLength = translatedRole.length;
+      let i = roleLength;
+      const deleteInterval = setInterval(() => {
+        if (i > 0) {
+          this.currentRole = translatedRole.slice(0, i - 1);
+          i--;
+        } else {
+          clearInterval(deleteInterval);
+          this.currentRoleIndex++;
+          if (this.currentRoleIndex === this.rolesHeader.length) {
+            this.currentRoleIndex = 0;
+          }
+          setTimeout(() => {
+            this.typeWriter();
+          }, 1000);
+        }
+      }, 50);
+    });
+  }
 }
