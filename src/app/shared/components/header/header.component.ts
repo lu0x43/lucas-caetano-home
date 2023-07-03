d7dbd66bc2ec6b72b9d3411914415c115244aa91
LucasCaetano <lucas_caetano123@hotmail.com>
2023-07-03 00:18:40 -0300

    refact type writer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -14,6 +14,8 @@ export class HeaderComponent implements OnInit {
   rolesHeader: string[] = ['ROLES.WEB_DEV', 'ROLES.MOBILE_DEV'];
   currentRoleIndex: number = 0;
   currentRole: string = '';
+  private readonly TYPE_INTERVAL = 100;
+  private readonly DELETE_INTERVAL = 50;
 
   constructor(
     public translateService: TranslateService,
@@ -37,42 +39,50 @@ export class HeaderComponent implements OnInit {
   typeWriter(): void {
     const roleKey = this.rolesHeader[this.currentRoleIndex];
     this.translateService.get(roleKey).subscribe((translatedRole: string) => {
-      const roleLength = translatedRole.length;
-      let i = 0;
-      const typeInterval = setInterval(() => {
-        if (i < roleLength) {
-          this.currentRole = translatedRole.slice(0, i + 1);
-          i++;
-        } else {
-          clearInterval(typeInterval);
-          setTimeout(() => {
-            this.deleteWriter();
-          }, 1000);
-        }
-      }, 100);
+      this.typeEffect(translatedRole);
     });
   }
 
   deleteWriter(): void {
     const roleKey = this.rolesHeader[this.currentRoleIndex];
     this.translateService.get(roleKey).subscribe((translatedRole: string) => {
-      const roleLength = translatedRole.length;
-      let i = roleLength;
-      const deleteInterval = setInterval(() => {
-        if (i > 0) {
-          this.currentRole = translatedRole.slice(0, i - 1);
-          i--;
-        } else {
-          clearInterval(deleteInterval);
-          this.currentRoleIndex++;
-          if (this.currentRoleIndex === this.rolesHeader.length) {
-            this.currentRoleIndex = 0;
-          }
-          setTimeout(() => {
-            this.typeWriter();
-          }, 1000);
-        }
-      }, 50);
+      this.deleteEffect(translatedRole);
     });
+  }
+
+  private typeEffect(translatedRole: string): void {
+    const roleLength = translatedRole.length;
+    let i = 0;
+    const typeInterval = setInterval(() => {
+      if (i < roleLength) {
+        this.currentRole = translatedRole.slice(0, i + 1);
+        i++;
+      } else {
+        clearInterval(typeInterval);
+        setTimeout(() => {
+          this.deleteWriter();
+        }, 2000);
+      }
+    }, this.TYPE_INTERVAL);
+  }
+
+  private deleteEffect(translatedRole: string): void {
+    const roleLength = translatedRole.length;
+    let i = roleLength;
+    const deleteInterval = setInterval(() => {
+      if (i > 0) {
+        this.currentRole = translatedRole.slice(0, i - 1);
+        i--;
+      } else {
+        clearInterval(deleteInterval);
+        this.currentRoleIndex++;
+        if (this.currentRoleIndex === this.rolesHeader.length) {
+          this.currentRoleIndex = 0;
+        }
+        setTimeout(() => {
+          this.typeWriter();
+        }, 2000);
+      }
+    }, this.DELETE_INTERVAL);
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       },
     },
     screens: {
-      xs: "300px",
+      xs: "280px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
