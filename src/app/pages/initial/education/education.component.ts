7524defb79bcfa11d9fbd4baf9e51db7a18a600c
LucasCaetano <lucas_caetano123@hotmail.com>
2023-07-25 18:50:05 -0300

    add education section

--- src/app/pages/initial/education/education.component.ts
+++ src/app/pages/initial/education/education.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Timeline } from 'src/app/core/model/timeline';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
+  title = 'EDUCATION.TITLE';
+  educationArray: Timeline[] = [
+    {
+      subTitle: 'EDUCATION.UTFPR',
+      date: 'EDUCATION.DATE_UTFPR',
+      text: 'EDUCATION.UTFPR_TXT',
+    },
+    {
+      subTitle: 'EDUCATION.ETEC',
+      date: 'EDUCATION.DATE_ETEC',
+      text: 'EDUCATION.ETEC_TXT',
+    },
+  ];
 
-    constructor() { }
+  constructor() {}
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {}
 }
