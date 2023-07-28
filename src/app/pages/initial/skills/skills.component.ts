afcb010a9651ca8e911bd9f3b84edd7a265d458b
LucasCaetano <lucas_caetano123@hotmail.com>
2023-07-28 17:44:22 -0300

    adjust navbar menu for mobile

--- src/app/pages/initial/skills/skills.component.ts
+++ src/app/pages/initial/skills/skills.component.ts
@@ -24,7 +24,7 @@ export class SkillsComponent implements OnInit {
   ];
   learning: string[] = [
     'Kotlin',
-    // 'swift',
+    // 'java',
     // 'CSharp',
     // 'ruby',
     'mysql',
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -7,21 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  navbarText: string[] = [
+  navbarPages: string[] = [
     'NAVBAR.HOME',
     'NAVBAR.SKILLS',
     'NAVBAR.CAREER',
-    // 'NAVBAR.EDUCATION',
+    'NAVBAR.EDUCATION',
     'NAVBAR.ABOUTME',
     'NAVBAR.PROJECTS',
   ];
   nameScroll: string[] = [];
+
+  openMenu: boolean = false;
 
   constructor(
     private viewPortScroller: ViewportScroller,
   ) {}
 
   ngOnInit(): void {}
+
+  changeStateMenu(): void {
+    this.openMenu = !this.openMenu;
+  }
 
   scrollComponent(page: string): void {
     this.nameScroll = page.split('.');
@@ -33,5 +39,7 @@ export class NavbarComponent implements OnInit {
     this.nameScroll.forEach((element) => {
       this.viewPortScroller.scrollToAnchor(element);
     });
+
+    this.openMenu = false;
   }
 }
